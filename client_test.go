c8c7167de0d18a5c606d45f7dbb833e41ad72abb
disaster37 <linuxworkgroup@hotmail.com>
2025-09-10 09:33:38 +0000

    fix: test
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- client_test.go
+++ client_test.go
@@ -16,6 +16,7 @@ func TestNewClient(t *testing.T) {
 	}
 
 	client, err := NewClient(cfg)
+	// Auth not work because it need centreon server
 	assert.Error(t, err)
-	assert.NotNil(t, client)
+	assert.Nil(t, client)
 }
